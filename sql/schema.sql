2eb0f9d12a1e355dc12e6b51f74234cfcd43dc98
jeni33 <168066781+jeni33@users.noreply.github.com>
2024-10-15 01:26:54 +0800

    Create schema.sql

---
+++ sql/schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE veterinarians_record (
+    vet_id INT AUTO_INCREMENT PRIMARY KEY,          
+    first_name VARCHAR(50),                
+    last_name VARCHAR(50) ,           
+    email VARCHAR(50) ,            
+    phone_number VARCHAR(20),                      
+    clinic_name VARCHAR(50),
+    specialization VARCHAR(50) ,                   
+    license_number VARCHAR(50)
+);
